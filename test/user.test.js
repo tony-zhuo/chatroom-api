178c2e52a98b4cc5f29706d5466931f698c56952
tony <tony860725@gmail.com>
2020-01-08 16:11:13 +0800

    add test

--- test/user.test.js
+++ test/user.test.js
@@ -2,11 +2,79 @@ const app = require('../app');
 const chai = require('chai');
 const request = require('supertest');
 
-describe('Login', () => {
+let APItoken;
+
+describe('user/login', () => {
   it('responds with json', done => {
     request(app)
       .post('user/login')
       .send({ email: 'admin_tony@gmail.com', password: '123456789' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.status.should.equal(201);
+        res.status.should.equal('success');
+        APItoken = res.token;
+        done();
+      });
+  });
+});
+
+describe('user/all', () => {
+  it('responds with json', done => {
+    request(app)
+      .post('user/all')
+      .set('Authorization', `Bearer ${APItoken}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.status.should.equal(201);
+        res.status.should.equal('success');
+        done();
+      });
+  });
+});
+
+describe('user/me', () => {
+  it('responds with json', done => {
+    request(app)
+      .post('user/me')
+      .set('Authorization', `Bearer ${APItoken}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.status.should.equal(201);
+        res.status.should.equal('success');
+        done();
+      });
+  });
+});
+
+describe('msg/all', () => {
+  it('responds with json', done => {
+    request(app)
+      .post('msg/all')
+      .set('Authorization', `Bearer ${APItoken}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.status.should.equal(201);
+        res.status.should.equal('success');
+        done();
+      });
+  });
+});
+
+describe('msg/send', () => {
+  it('responds with json', done => {
+    request(app)
+      .post('msg/all')
+      .set('Authorization', `Bearer ${APItoken}`)
+      .send({ text: 'unit test' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .end((err, res) => {
